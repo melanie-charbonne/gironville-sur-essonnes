e3ab3f65e2c83213a28c6e2ac9a8db8a23cacbb2
melanie-go <melanie.go@hagergroup.com>
2022-11-28 10:32:18 +0100

    feat: add Button component

---
+++ components/ButtonMain/ButtonMain.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import classNames from 'classnames'
+import styles from './ButtonMain.module.scss'
+import ArrowLeft from '../../asset/images/icons/arrow-left.svg'
+
+type ButtonMain = {
+    arrow?: Boolean
+    secondary?: Boolean
+    color?: string
+}
+
+export default function ButtonMain({ arrow, secondary, color }: ButtonMain) {
+    return (
+        <>
+            <Link href="actualites">
+                <div className="flex justify-center">
+                    <button
+                        className={classNames(
+                            `${styles.button}`,
+                            'button',
+                            'text-center',
+                            'flex',
+                            'justify-center',
+                            'items-center',
+                            'px-6',
+                            'py-3',
+                            {
+                                [styles.secondary]: secondary,
+                                [styles.light]: color === 'light',
+                                [styles.arrow]: arrow,
+                            }
+                        )}
+                    >
+                        <>
+                            Toute l'actualité
+                            {arrow && <ArrowLeft className="ml-2" />}
+                        </>
+                    </button>
+                </div>
+            </Link>
+        </>
+    )
+}
---
+++ components/PostCard/PostCard.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import CoverImage from '../CoverImage'
+import PostDate from '../PostDate'
+import styles from './PostCard.module.scss'
+
+type Post = {
+    uri: string
+    featuredImage: {
+        node: {
+            altText: string
+            sourceUrl: string
+        }
+    }
+    title: string
+    date: Date
+}
+type PostCardProps = {
+    post: Post
+    width?: number
+    height?: number
+    layout?: string
+}
+export default function PostCard({
+    post,
+    width,
+    height,
+    layout,
+}: PostCardProps) {
+    return (
+        <Link href={`actualites${post.uri}`}>
+            <article
+                className={`${styles.card} latest-posts-item card hover:cursor-pointer hover:cursor-pointer`}
+            >
+                {post.featuredImage && (
+                    <CoverImage
+                        width={width}
+                        height={height}
+                        title={post.title}
+                        featuredImage={post.featuredImage}
+                        layout={layout}
+                        classNames={'rounded-md transition duration-250'}
+                    />
+                )}
+                <PostDate date={post.date} />
+                <h3>{post.title}</h3>
+            </article>
+        </Link>
+    )
+}
