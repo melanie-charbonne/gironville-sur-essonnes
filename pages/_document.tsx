5c465ceaf8389af306d34792c6c08466d0d95ee8
Mélanie Charbonné <melanie@192.168.1.29>
2023-08-15 20:29:42 +0200

    feat: add js depencies for elementor

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,23 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
                 />
                 <Script
+                    src="http://gironville-sur-essonne.local/wp-includes/js/jquery/jquery.min.js"
+                    strategy="lazyOnload"
+                />
+                <Script
+                    src="http://gironville-sur-essonne.local/wp-content/plugins/elementor/assets/js/frontend.js"
+                    strategy="lazyOnload"
+                />
+                <Script
+                    src="http://gironville-sur-essonne.local/wp-content/plugins/elementskit-lite/widgets/init/assets/js/elementor.js"
+                    strategy="lazyOnload"
+                />
+                <Script
                     src="http://gironville-sur-essonne.local/wp-content/plugins/elementskit-lite/widgets/init/assets/js/widget-scripts.js?ver=2.9.0"
+                    strategy="lazyOnload"
+                />
+                <Script
+                    src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"
                     strategy="lazyOnload"
                 />
             </Head>
