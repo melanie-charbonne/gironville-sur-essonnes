1b46292c44b61b60737ce954a4b1d7a56515db73
Mélanie Charbonné <melanie@192.168.1.21>
2023-08-28 18:29:03 +0200

    feat: update PER_PAGE_FIRST to 12

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 // Pagination
-export const PER_PAGE_FIRST = 3 // Nb of posts to be shown on first page.
+export const PER_PAGE_FIRST = 12 // Nb of posts to be shown on first page.
 export const PER_PAGE_REST = 18 // Nb of posts to be shown following page and after.
